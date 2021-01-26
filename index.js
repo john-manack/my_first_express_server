6e903394dfbbac82e620a0dd6673a52880eb6b1a
John Manack <jmnck28@gmail.com>
2021-01-26 14:49:45 -0500

    updates index.js file to include server listener

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const HOSTNAME = '127.0.0.1';
 const PORT = 3000;
 
 const express = require('express');
+const app = express();
+
+const SERVER = HTTP.createServer(app);
+
+SERVER.listen(PORT, HOSTNAME, function () {
+    // We're using this console.log to let us know that the server is running.
+    console.log(`Server is running at http://${HOSTNAME}:${PORT}/`);
+});
