ce7f0df7be617ecc937db43e882b9cebf9606718
John Manack <jmnck28@gmail.com>
2021-01-26 15:10:39 -0500

    adds root and /friends paths

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ SERVER.listen(PORT, HOSTNAME, function () {
     // We're using this console.log to let us know that the server is running.
     console.log(`Server is running at http://${HOSTNAME}:${PORT}/`);
 });
+
+// the '/' below is known as the "root route" :D
+app.get('/', function (req, res) {
+    res.send('Hello from Express');
+});
+
+// "root route" should be written first, followed by all subsequent routes (eg. "/friends" below)
+app.get('/friends', function (req, res) {
+    res.send('This will be a friends list');
+});
