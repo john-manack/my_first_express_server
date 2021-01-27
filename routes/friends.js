2df65538a7d911831632029d9bd194a509cebc8d
John Manack <jmnck28@gmail.com>
2021-01-27 11:55:43 -0500

    includes route parameters

--- routes/friends.js
+++ routes/friends.js
@@ -8,10 +8,25 @@ const friendsArray = require('../db')
 router.get('/', (req, res) => {
     let htmlData = `<ul>`;
     friendsArray.map((friend) => {
-        htmlData += `<li>${friend.name}</li>`
+        htmlData += `<li><a href="./friends/${friend.handle}">${friend.name}</a></li>`
     })
     htmlData += `</ul> `;
     res.send(htmlData);
 });
+
+// The colon below in the first argument of the function denotes a parameter, and makes 'handle' available as a variable to call with 'req.params.handle'.
+router.get('/:handle', (req, res) => {
+    const { handle } = req.params;
+    const friend = friendsArray.find((friend) => {
+        if (friend.handle === handle) {
+            return friend;
+        }
+    });
+    if (friend) {
+        res.send(`<h1>Hi ${friend.name}!</h1>`)
+    } else {
+        res.send(`No friend with the handle, ${handle}, was found :(`)
+    }
+})
 
 module.exports = router;
